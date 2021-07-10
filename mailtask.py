c736184cf50de7dca3abec0e1c858d1dac9bffdf
Ajay pediredla <57003507+jay-498@users.noreply.github.com>
2021-07-10 22:42:35 +0530

    Update mailtask.py

--- mailtask.py
+++ mailtask.py
@@ -3,11 +3,12 @@
 import smtplib
 from email.message import EmailMessage
 
-
+#function to send the email using smtplib library
 def send_email(receiver, subject, message):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login('ajay.pediredla.1818@gmail.com', '8143232797')
+    #here you have to give sender email and password
+    server.login('ajay.pediredla.1818@gmail.com', '**********')
     email = EmailMessage()
     email['From'] = 'ajay.pediredla.1818@gmail.com'
     email['To'] = receiver
