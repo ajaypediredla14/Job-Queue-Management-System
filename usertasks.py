c90c6104c8ba993c435725e0739f6c7a0c605ac2
Ajay pediredla <57003507+jay-498@users.noreply.github.com>
2021-07-10 22:43:51 +0530

    Update usertasks.py

--- usertasks.py
+++ usertasks.py
@@ -9,12 +9,13 @@ conn.execute('''CREATE TABLE IF NOT EXISTS todo(
     message TEXT 
 	);''')
 
-
+#function to show pass the data to the populate(task_kinter.py) function 
 def show():
     query = "SELECT * FROM todo;"
     rows=conn.execute(query)
     return rows
 
+#function to insert the data to the database
 def insertdata(email,subject,message):
     rs=conn.cursor()
     query = "INSERT INTO todo(email,subject,message) VALUES(?,?,?)"
@@ -23,7 +24,7 @@ def insertdata(email,subject,message):
     conn.commit()
 
 
-
+#function to delete the task
 def deletebytask(taskval):
     query = "DELETE FROM todo WHERE email = ? AND subject=? AND message=?;"
     conn.execute(query,taskval)
